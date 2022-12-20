d88d91ea31c2d3a84fb1e7d4a5e51525ba6d1018
Alexey <alexeysitiy@gmail.com>
2022-12-20 17:51:21 +0200

    add JS slider for PC

--- src/js/modules/slider.js
+++ src/js/modules/slider.js
@@ -2,17 +2,28 @@ const slider = () => {
     const sliderWrapper = document.querySelector(".slider__slides"),
         sliderInner = document.querySelector(".slider__inner"),
         slides = document.querySelectorAll(".slider__item"),
+        indicatorWrapper = document.querySelector(".slider__indicators"),
         prev = document.querySelector(".slider__prev"),
         next = document.querySelector(".slider__next"),
-        contentWidth = sliderInner.clientWidth + "px" //?????????????
+        contentWidth = sliderInner.clientWidth + "px";
 
 
-    let offset = 0;
-    let currentSlide = 0;
+    let offset = 0,
+        currentSlide = 0,
+        currentIndicator = 0;
 
+
+    for (let i = 0; i < slides.length; i++) {
+        const indicator = document.createElement("li");
+        if (i === currentSlide) {
+            indicator.classList.add("active");
+        }
+        indicator.setAttribute("data-slide-to", `${i}`);
+        indicatorWrapper.append(indicator);
+    }
+
+    const indicators = document.querySelectorAll(".slider__indicators > li");
     sliderWrapper.style.width = slides.length * 100 + "%";
-
-    console.log(slides.length);
 
     slides.forEach(item => {
         item.style.width = contentWidth;
@@ -20,6 +31,15 @@ const slider = () => {
 
     function doSlideMove() {
         sliderWrapper.style.transform = `translateX(-${offset}px)`;
+
+        indicators.forEach((item, index) => {
+            if (index === currentIndicator) {
+                item.classList.add("active");
+            } else {
+                item.classList.remove("active");
+            }
+
+        })
     }
 
 
@@ -29,13 +49,14 @@ const slider = () => {
         if (offset >= parseInt(contentWidth) * (slides.length - 1)) {
             offset = 0;
             currentSlide = 0;
+            currentIndicator = 0;
         } else {
             offset += parseInt(contentWidth);
             currentSlide++;
+            currentIndicator++;
         }
 
         doSlideMove();
-
     });
 
     prev.addEventListener("click", (e) => {
@@ -44,14 +65,26 @@ const slider = () => {
         if (offset === 0) {
             offset = parseInt(contentWidth) * (slides.length - 1);
             currentSlide = slides.length - 1;
+            currentIndicator = slides.length - 1
         } else {
             offset -= parseInt(contentWidth);
             currentSlide--;
+            currentIndicator--;
         }
 
         doSlideMove();
-
     });
+
+    indicators.forEach(item => {
+        item.addEventListener("click", (e) => {
+            const ind = parseInt(item.getAttribute("data-slide-to"));
+            currentIndicator = ind;
+            currentSlide = ind;
+            offset = currentSlide * parseInt(contentWidth);
+            doSlideMove();
+        });
+    })
+
 
 }
 
