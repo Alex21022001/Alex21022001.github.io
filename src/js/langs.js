1c919a04486d526e594ad07b4c5adb825a67124e
Alexey <alexeysitiy@gmail.com>
2023-01-02 18:16:57 +0200

    fix langs js file for aboutUs section

--- src/js/langs.js
+++ src/js/langs.js
@@ -1,106 +1,47 @@
 const allLangs = {
     /// PROMO ///
-    "promo .promo__text-title":{
+    "promo .promo__text-title": {
         "ua": "Українська команда трансферу технологій" + "<br>(UTTTLLC)",
     },
-    "promo .promo__text-subtitle":{
+    "promo .promo__text-subtitle": {
         "ua": "Високотехнологічні рішення для створення бізнес-моделей лідерів ринку",
     },
-    "promo .promo__text-description":{
+    "promo .promo__text-description": {
         "ua": "Майстерність монетизації ідей і смислів.<br> \n" +
             "Синергія результатів і корисності для світу. <br>\n" +
             "Фокус - можливості та прибуток. <br>\n" +
             "Концепція - тотальне домінування на ринку.",
     },
-    // "promo__text-title": {
-    //     "ua": "Українська команда трансферу технологій" + "<br>(UTTTLLC)",
-    //     "en": "Ukrainian Technology Transfer Team <br>(UTTT LLC)"
-    // },
-    // "promo__text-subtitle": {
-    //     "ua": "Високотехнологічні рішення для створення бізнес-моделей лідерів ринку",
-    //     "en": "High-Tech Solutions For Creating Market Leaders' Business Models"
-    // },
-    // "promo__text-description": {
-    //     "ua": "Майстерність монетизації ідей і смислів.<br> \n" +
-    //         "Синергія результатів і корисності для світу. <br>\n" +
-    //         "Фокус - можливості та прибуток. <br>\n" +
-    //         "Концепція - тотальне домінування на ринку.",
-    //     "en": "Mastery for monetizing ideas and meanings.<br>" +
-    //         "Synergy of results and usefulness to the world. <br>" +
-    //         "Focus - opportunities and profit. <br>" +
-    //         "Concept - total market dominance."
-    // },
-    "promo .menu-about":{
+    "promo .menu-about": {
         "ua": "Про нас",
     },
-    "promo .menu-projects":{
+    "promo .menu-projects": {
         "ua": "Проєкти",
     },
-    "promo .menu-team":{
+    "promo .menu-team": {
         "ua": "Команда",
     },
-    "promo .menu-partners":{
+    "promo .menu-partners": {
         "ua": "Наші партнери",
     },
-    "promo .menu-contacts":{
+    "promo .menu-contacts": {
         "ua": "Контакти",
     },
-    // "menu__about": {
-    //     "ua": "Про Нас",
-    //     "en": "About Us"
-    // },
-    // "menu__projects": {
-    //     "ua": "Проєкти",
-    //     "en": "Projects"
-    // },
-    // "menu__team": {
-    //     "ua": "Команда",
-    //     "en": "Team"
-    // },
-    // "menu__partners": {
-    //     "ua": "Наші Партнери",
-    //     "en": "Our Partners"
-    // },
-    // "menu__contacts": {
-    //     "ua": "Контакти",
-    //     "en": "Contacts"
-    // }, "mb_menu__about": {
-    //     "ua": "Про Нас",
-    //     "en": "About Us"
-    // },
-    // "mb_menu__projects": {
-    //     "ua": "Проєкти",
-    //     "en": "Projects"
-    // },
-    // "mb_menu__team": {
-    //     "ua": "Команда",
-    //     "en": "Team"
-    // },
-    // "mb_menu__partners": {
-    //     "ua": "Наші Партнери",
-    //     "en": "Our Partners"
-    // },
-    // "mb_menu__contacts": {
-    //     "ua": "Контакти",
-    //     "en": "Contacts"
-    // },
-    "aboutUs__title": {
+
+    /// About Us ///
+
+    "aboutUs .aboutUs__title": {
         "ua": "Про нас",
-        "en": "About Us"
     },
-    "aboutUs__description": {
+    "aboutUs .lang-aboutUs__description": {
         "ua":
             "<span>Ukrainian Technology Transfer Team (UTTT LLC) Молода українська компанія ТОВ \"Юкрейніан Текнолоджи Трансфер Тим\", що динамічно розвивається.</span>" +
             "<span>Основна діяльність спрямована на бізнес-консалтинг,розробку, підтримку та імплементацію, масштабування інноваційних технологічних проектів і стартапів.</span>",
-        "en":
-            "<span>Ukrainian Technology Transfer Team (UTTT LLC) We are a young dynamically developing Ukrainian company ТОВ \"Юкрейніан Текнолоджи Трансфер Тим\".</span>" +
-            "<span>The main activity is business consulting, development, support and implementation, and scaling of innovative technological projects and startups.</span>"
     },
-    "aboutUs__more-information": {
+    "aboutUs .aboutUs__more-information": {
         "ua": "Більше про нас",
-        "en": "More information"
     },
-    "aboutUs__description_additional": {
+    "aboutUs .aboutUs__description_additional": {
         "ua":
             "<br>" +
             "<b>Наша команда</b> &ndash; це досвідчені фахівці, науковці, практики та експерти, які знають тонкощі своєї справи." +
@@ -112,25 +53,9 @@ const allLangs = {
             "рентабельністю, масштабованістю та прикладною значущістю. Ми ті, хто покращить життя на Землі екологічно, системно і етично." +
             "<br><br>\n" +
             "<b>Наша місія</b> &ndash; розвиток та поширення ідеології свідомого соціально-орієнтованого бізнесу в глобальній інтегрованій екосистемі світу.",
-
-        "en":
-            "<br>" +
-            "<b>Our team</b> is experienced specialists, scientists, practitioners, and experts who know the\n" +
-            "key points of their business." +
-            "<br><br>" +
-            "<b>Our goal</b> is to make people healthier, the planet ecological and safe, develop and\n" +
-            "implement modern innovative technologies, preserve the environment, improving people's comfort,\n" +
-            "safety, quality, and living standards.\n" +
-            "<br><br>\n" +
-            "<b>The company's policy</b> is to offer the consumer a high-tech useful product and for\n" +
-            "investors projects with high profitability, scalability, and applied significance. We are the\n" +
-            "ones who going to improve life on Earth ecologically, systematically, and ethically.\n" +
-            "<br><br>\n" +
-            "<b>Our mission</b> is the development and spread of the conscious socially oriented business\n" +
-            "ideology in the globally integrated ecosystem of the world."
     },
 
-
+    /// PROJECTS ///
     "projects .projects__title": {
         "ua": "Проєкти"
     },
